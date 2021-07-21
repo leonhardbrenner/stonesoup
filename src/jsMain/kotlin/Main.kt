4d89fa1841619fa98f64fd1150d9ad4c9ee5c7eb
Leonhard Brenner <lbrenner@execvision.io>
2021-07-21 15:21:27 -0400

    fix-mui - Fixes the icons by adding a link for the stylesheet.

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,17 +1,24 @@
+import com.ccfraser.muirwik.components.mThemeProvider
+import com.ccfraser.muirwik.components.styles.mStylesProvider
 import react.dom.render
 import kotlin.browser.document
 
-//import kotlinx.browser.document
+import kotlinx.browser.document
 
 //TODO - https://github.com/gaearon/react-hot-loader
-//@JsModule("react-hot-loader")
-//private external val hotModule: dynamic
-//private val hot = hotModule.hot
-//private val module = js("module")
+@JsNonModule
+@JsModule("react-hot-loader")
+private external val hotModule: dynamic
+private val hot = hotModule.hot
+private val module = js("module")
 
 fun main() {
     render(document.getElementById("root")) {
-        //val hotWrapper = hot(module)
-        app()
+        val hotWrapper = hot(module)
+        mStylesProvider("jss-insertion-point") {
+            mThemeProvider {
+                hotWrapper(app())
+            }
+        }
     }
 }
