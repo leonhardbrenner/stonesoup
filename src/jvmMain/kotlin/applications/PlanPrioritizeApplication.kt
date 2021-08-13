920f26502935f7381b0cb5bc8c43e357db97e7e0
Leonhard Brenner <lbrenner@execvision.io>
2021-08-13 08:48:20 -0400

    delete - Wired the GET to chore table.

--- src/jvmMain/kotlin/applications/PlanPrioritizeApplication.kt
+++ src/jvmMain/kotlin/applications/PlanPrioritizeApplication.kt
@@ -5,7 +5,9 @@ import com.mongodb.ConnectionString
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
-import models.Chore
+//import models.Chore
+import generated.model.Seeds.Chore
+import generated.model.SeedsDto
 import org.litote.kmongo.eq
 import javax.inject.Inject
 import io.ktor.application.*
@@ -18,6 +20,7 @@ import models.NodeUpdate
 import org.litote.kmongo.addToSet
 import org.litote.kmongo.pull
 import org.litote.kmongo.setValue
+import services.SeedsService
 import java.util.*
 
 //Moving on I need to fix my id. I will move to name=path. This gives me assigned vs unassigned.
@@ -27,31 +30,31 @@ class PlanPrioritizeApplication @Inject constructor(val service: Service, val da
         get() = database.getCollection<Chore>()
 
     fun routesFrom(routing: Routing) = routing.apply {
-        route(Chore.path) {
+        route(SeedsDto.Chore.path) {
 
             get {
                 //call.respond(collection.find().toList())
                 call.respond(service.get())
             }
 
-            post {
-                val item = call.receive<ChoreCreate>()
-                service.add(item)
-                call.respond(HttpStatusCode.OK)
-            }
-
-            //XXX -- none of this works anymore. figure out the mongo api.
-            put("/{id}") {
-                val item = call.receive<NodeUpdate>()
-                service.update(item)
-                call.respond(HttpStatusCode.OK)
-            }
-
-            delete("/{id}") {
-                val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
-                service.delete(id)
-                call.respond(HttpStatusCode.OK)
-            }
+            //post {
+            //    val item = call.receive<ChoreCreate>()
+            //    service.add(item)
+            //    call.respond(HttpStatusCode.OK)
+            //}
+            //
+            ////XXX -- none of this works anymore. figure out the mongo api.
+            //put("/{id}") {
+            //    val item = call.receive<NodeUpdate>()
+            //    service.update(item)
+            //    call.respond(HttpStatusCode.OK)
+            //}
+            //
+            //delete("/{id}") {
+            //    val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+            //    service.delete(id)
+            //    call.respond(HttpStatusCode.OK)
+            //}
         }
     }
 
@@ -73,64 +76,71 @@ class PlanPrioritizeApplication @Inject constructor(val service: Service, val da
 
     }
 
-    class Service @Inject constructor(val database: CoroutineDatabase) {
+    class Service @Inject constructor(
+        val database: CoroutineDatabase,
+        val seedsService: SeedsService) {
 
-        val collection
-            get() = database.getCollection<Chore>()
+        //val collection
+        //    get() = database.getCollection<Chore>()
 
-        suspend fun get(): List<Chore> {
+        //suspend fun get(): List<Chore> {
+        //    //Reinsert root.
+        //    //collection.insertOne(Chore(name = "<root>", id = 0, parentId = -1))
+        //    return collection.find().toList()
+        //}
+        fun get(): List<Chore> {
             //Reinsert root.
             //collection.insertOne(Chore(name = "<root>", id = 0, parentId = -1))
-            return collection.find().toList()
+            return seedsService.getChores()
         }
 
-        suspend fun add(item: ChoreCreate) {
-            val chore = Chore((Date().time/1000).toInt(), parentId = 0, listOf(), item.name, item.description, item.priority, item.estimateInHours)
-            collection.updateOne(
-                Chore::id eq chore.parentId,
-                addToSet(Chore::childrenIds, chore.id)
-            )
-            collection.insertOne(chore)
-        }
-
-        /**
-         * Todo
-         *   the node id should then be used for our update
-         *   let's start with move then do link
-         *   display on the front end more like a graph
-         *   create a link routine something as simple as little x(es) that connect
-         *   make a priority widget something like a +/-
-         *   make a box for real description
-         *   we need a field for time estimates
-         *   move to tornadoFx
-         */
-        suspend fun update(item: NodeUpdate) {
-            collection.updateOne(
-                Chore::id eq parent(item.id),
-                pull(Chore::childrenIds, item.id!!)
-            )
-            collection.updateOne(
-                Chore::id eq item.moveTo,
-                addToSet(Chore::childrenIds, item.id!!)
-            )
-            collection.updateOne(
-                Chore::id eq item.id,
-                setValue(Chore::parentId, item.moveTo!!)
-            )
-        }
-        //Todo - make this non-nullable
-        suspend fun element(id: Int) =
-            collection.findOne(Chore::id eq id)
-
-        suspend fun parent(id: Int) =
-            element(id)!!.parentId
-
-        suspend fun delete(id: Int) {
-            collection.updateOne(
-                Chore::id eq parent(id),
-                pull(Chore::childrenIds, id)
-            )
-            collection.deleteMany(Chore::id eq id)
-        }
+        //suspend fun add(item: ChoreCreate) {
+        //    val chore = Chore((Date().time/1000).toInt(), parentId = 0, listOf(), item.name, item.description, item.priority, item.estimateInHours)
+        //    collection.updateOne(
+        //        Chore::id eq chore.parentId,
+        //        addToSet(Chore::childrenIds, chore.id)
+        //    )
+        //    collection.insertOne(chore)
+        //}
+        //
+        ///**
+        // * Todo
+        // *   the node id should then be used for our update
+        // *   let's start with move then do link
+        // *   display on the front end more like a graph
+        // *   create a link routine something as simple as little x(es) that connect
+        // *   make a priority widget something like a +/-
+        // *   make a box for real description
+        // *   we need a field for time estimates
+        // *   move to tornadoFx
+        // */
+        //suspend fun update(item: NodeUpdate) {
+        //    collection.updateOne(
+        //        Chore::id eq parent(item.id),
+        //        pull(Chore::childrenIds, item.id!!)
+        //    )
+        //    collection.updateOne(
+        //        Chore::id eq item.moveTo,
+        //        addToSet(Chore::childrenIds, item.id!!)
+        //    )
+        //    collection.updateOne(
+        //        Chore::id eq item.id,
+        //        setValue(Chore::parentId, item.moveTo!!)
+        //    )
+        //}
+        ////Todo - make this non-nullable
+        //suspend fun element(id: Int) =
+        //    collection.findOne(Chore::id eq id)
+        //
+        //suspend fun parent(id: Int) =
+        //    element(id)!!.parentId
+        //
+        //suspend fun delete(id: Int) {
+        //    collection.updateOne(
+        //        Chore::id eq parent(id),
+        //        pull(Chore::childrenIds, id)
+        //    )
+        //    collection.deleteMany(Chore::id eq id)
+        //}
     }
 }
