2af6792f19afc2dff4f5fe4e5061776a838e09c2
Leonhard Brenner <lbrenner@execvision.io>
2021-09-05 15:25:30 -0400

    crud - Dunno

--- src/jvmMain/kotlin/applications/routing/DetailedSeedsRouting.kt
+++ src/jvmMain/kotlin/applications/routing/DetailedSeedsRouting.kt
@@ -1,53 +1,19 @@
-package applications
+package applications.routing
 
-import applications.routing.ChoreRouting
-import com.google.inject.AbstractModule
+import applications.CoreApplication
 import generated.model.SeedsDto
-import javax.inject.Inject
 import io.ktor.application.*
-import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.response.*
-import services.SeedsService
-import services.crud.ChoreDao
+import io.ktor.routing.*
+import javax.inject.Inject
+import kotlin.text.get
 
-class CoreApplication @Inject constructor(
-    val dao: Dao,
-    val seedsService: SeedsService,
-    val choreRouting: ChoreRouting
-    ) {
+class DetailedSeedsRouting @Inject constructor(val dao: CoreApplication.Dao) {
+    fun routes(routing: Routing) = routing.route(SeedsDto.Chore.path) {
 
-    fun routesFrom(routing: Routing) {
-        routesFromMySeeds(routing)
-        routesFromDetailedSeed(routing)
-        routesFromSeedCategory(routing)
-        choreRouting.routes(routing)
-    }
-
-    //https://ktor.io/docs/routing-in-ktor.html#define_route
-    //https://medium.com/@shubhangirajagrawal/the-7-restful-routes-a8e84201f206
-    fun routesFromMySeeds(routing: Routing) = routing.route(SeedsDto.MySeeds.path) {
-        get {
-            //XXX - You will need an outbound route which creates a Dto for us. This would be a good use of extensions.
-            call.respond(seedsService.mySeeds)
-        }
-    }
-
-    fun routesFromDetailedSeed(routing: Routing) = routing.route(SeedsDto.DetailedSeed.path) {
-        get {
-            call.respond(seedsService.getDetailedSeeds())
-        }
-    }
-
-    fun routesFromSeedCategory(routing: Routing) = routing.route(SeedsDto.SeedCategory.path) {
-        get {
-            call.respond(seedsService.getCategories())
-        }
-    }
-    fun routesFromChore(routing: Routing) = routing.route(SeedsDto.Chore.path) {
         get {
             //call.respond(collection.find().toList())
-            //Todo - figure out why this is red
             call.respond(dao.Chore.index())
         }
 
@@ -89,18 +55,5 @@ class CoreApplication @Inject constructor(
             dao.Chore.destroy(id)
             call.respond(HttpStatusCode.OK)
         }
-    }
-
-    object Module : AbstractModule() {
-
-        override fun configure() {
-            //Stonesoup PR 10 removed the rest database and all MongoDB wiring.
-            //bind(CoroutineDatabase::class.java).toInstance(database())
-        }
-
-    }
-
-    class Dao {
-        val Chore = ChoreDao
     }
 }
