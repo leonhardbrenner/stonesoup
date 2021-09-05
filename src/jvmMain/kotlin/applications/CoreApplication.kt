e6710e7fdbe5dab212e33a3789ba7db07239bc23
Leonhard Brenner <lbrenner@execvision.io>
2021-09-05 15:13:08 -0400

    crud - Refactor: Split methods up.

--- src/jvmMain/kotlin/applications/CoreApplication.kt
+++ src/jvmMain/kotlin/applications/CoreApplication.kt
@@ -12,72 +12,77 @@ import services.crud.ChoreDao
 
 class CoreApplication @Inject constructor(val dao: Dao, val seedsService: SeedsService) {
 
+    fun routesFrom(routing: Routing) = routing.apply {
+        routesFromMySeeds()
+        routesFromDetailedSeed()
+        routesFromSeedCategory()
+        routesFromChore()
+    }
+
     //https://ktor.io/docs/routing-in-ktor.html#define_route
     //https://medium.com/@shubhangirajagrawal/the-7-restful-routes-a8e84201f206
-    fun routesFrom(routing: Routing) = routing.apply {
-        route(SeedsDto.MySeeds.path) {
-            get {
-                //XXX - You will need an outbound route which creates a Dto for us. This would be a good use of extensions.
-                call.respond(seedsService.mySeeds)
-            }
+    fun Routing.routesFromMySeeds() = route(SeedsDto.MySeeds.path) {
+        get {
+            //XXX - You will need an outbound route which creates a Dto for us. This would be a good use of extensions.
+            call.respond(seedsService.mySeeds)
         }
-        route(SeedsDto.DetailedSeed.path) {
-            get {
-                call.respond(seedsService.getDetailedSeeds())
-            }
+    }
+
+    fun Routing.routesFromDetailedSeed() = route(SeedsDto.DetailedSeed.path) {
+        get {
+            call.respond(seedsService.getDetailedSeeds())
         }
-        route(SeedsDto.SeedCategory.path) {
-            get {
-                call.respond(seedsService.getCategories())
-            }
+    }
+
+    fun Routing.routesFromSeedCategory() = route(SeedsDto.SeedCategory.path) {
+        get {
+            call.respond(seedsService.getCategories())
         }
-        route(SeedsDto.Chore.path) {
-
-            get {
-                //call.respond(collection.find().toList())
-                call.respond(dao.Chore.index())
-            }
-
-            //get("/new") {
-            //    TODO("Show form to make new Chore")
-            //    //call.respond(collection.find().toList())
-            //    //call.respond(dao.Chore.index())
-            //}
-
-            post {
-                val parentId = call.parameters["parentId"]?.toInt() ?: return@post call.respond(HttpStatusCode.BadRequest)
-                val name = call.parameters["name"] ?: return@post call.respond(HttpStatusCode.BadRequest)
-                dao.Chore.create(parentId, name)
-                call.respond(HttpStatusCode.OK)
-            }
-
-            //get("/{id}") {
-            //    TODO("Show form to make new Chore")
-            //    //call.respond(collection.find().toList())
-            //    //call.respond(dao.Chore.index())
-            //}
-
-            //get("/{id}/edit") {
-            //    TODO("Show form to make new Chore")
-            //    //call.respond(collection.find().toList())
-            //    //call.respond(dao.Chore.index())
-            //}
-
-            put("/{id}") {
-                val id = call.parameters["id"]?.toInt() ?: return@put call.respond(HttpStatusCode.BadRequest)
-                val parentId = call.parameters["parentId"]?.toInt() ?: return@put call.respond(HttpStatusCode.BadRequest)
-                val name = call.parameters["name"]// ?: return@put call.respond(HttpStatusCode.BadRequest)
-                dao.Chore.update(id, parentId, name)
-                call.respond(HttpStatusCode.OK)
-            }
-
-            delete("/{id}") {
-                val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
-                dao.Chore.destroy(id)
-                call.respond(HttpStatusCode.OK)
-            }
+    }
+    fun Routing.routesFromChore() = route(SeedsDto.Chore.path) {
+        get {
+            //call.respond(collection.find().toList())
+            call.respond(dao.Chore.index())
         }
 
+        //get("/new") {
+        //    TODO("Show form to make new Chore")
+        //    //call.respond(collection.find().toList())
+        //    //call.respond(dao.Chore.index())
+        //}
+
+        post {
+            val parentId = call.parameters["parentId"]?.toInt() ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val name = call.parameters["name"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            dao.Chore.create(parentId, name)
+            call.respond(HttpStatusCode.OK)
+        }
+
+        //get("/{id}") {
+        //    TODO("Show form to make new Chore")
+        //    //call.respond(collection.find().toList())
+        //    //call.respond(dao.Chore.index())
+        //}
+
+        //get("/{id}/edit") {
+        //    TODO("Show form to make new Chore")
+        //    //call.respond(collection.find().toList())
+        //    //call.respond(dao.Chore.index())
+        //}
+
+        put("/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: return@put call.respond(HttpStatusCode.BadRequest)
+            val parentId = call.parameters["parentId"]?.toInt() ?: return@put call.respond(HttpStatusCode.BadRequest)
+            val name = call.parameters["name"]// ?: return@put call.respond(HttpStatusCode.BadRequest)
+            dao.Chore.update(id, parentId, name)
+            call.respond(HttpStatusCode.OK)
+        }
+
+        delete("/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+            dao.Chore.destroy(id)
+            call.respond(HttpStatusCode.OK)
+        }
     }
 
     object Module : AbstractModule() {
