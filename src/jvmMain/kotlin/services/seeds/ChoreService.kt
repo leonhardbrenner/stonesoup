1b5bcdd929d4af1641b63c76879701cba9a3a79f
Leonhard Brenner <lbrenner@execvision.io>
2021-09-10 13:24:46 -0400

    delete - dao uses lower case now.

--- src/jvmMain/kotlin/services/seeds/ChoreService.kt
+++ src/jvmMain/kotlin/services/seeds/ChoreService.kt
@@ -1,28 +1,26 @@
 package services.seeds
 
 import dao.SeedsDao
-import dao.seeds.ChoreDao
 import generated.model.Seeds
-import generated.model.SeedsDto
 import models.SeedsResources
 import javax.inject.Inject
 
 class ChoreService @Inject constructor(val dao: SeedsDao) {
 
     fun index() = let {
-        val chores = dao.Chore.index()
+        val chores = dao.chore.index()
         val childrenIds = chores
             .map { it.parentId to it.id }
             .groupBy({ it.first }, { it.second })
-        val schedules = dao.Schedule.index().associateBy { it.choreId }
+        val schedules = dao.schedule.index().associateBy { it.choreId }
         chores.map { SeedsResources.Chore(it, childrenIds[it.id]?: emptyList(), schedules[it.id]) }
     }
 
-    fun create(source: Seeds.Chore) = dao.Chore.create(source)
+    fun create(source: Seeds.Chore) = dao.chore.create(source)
 
-    fun update(source: Seeds.Chore) = dao.Chore.update(source)
+    fun update(source: Seeds.Chore) = dao.chore.update(source)
 
-    fun move(id: Int, parentId: Int) = update(dao.Chore.get(id).copy(parentId = parentId))
+    fun move(id: Int, parentId: Int) = update(dao.chore.get(id).copy(parentId = parentId))
 
-    fun destroy(id: Int) = dao.Chore.destroy(id)
+    fun destroy(id: Int) = dao.chore.destroy(id)
 }
